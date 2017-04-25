ed6a1baf1b966be384b95a07e4e0160a6be62da9
smideros <smideros@illinois.edu>
2017-04-25 09:17:21 -0500

    debug trimmomaticScript SE for permision denied error

--- trimmomaticScript_AX2331_SE.sh
+++ trimmomaticScript_AX2331_SE.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=1,mem=2gb
+#PBS -l nodes=1:ppn=2,mem=2gb
 #PBS -q default
 #PBS -j oe
-#PBS trimmomatic_SE
-#PBS -M mmsalaz2@illinois.edu
-#PBS -m be
 #----------------------------------------------------------------------------#
 module load java/1.8.0_65
 module load trimmomatic/0.36
 #----------------------------------------------------------------------------#
-cd /home/a-m/mmsalaz2/trimming/
+cd ~/trimming/
 java -jar /home/apps/trimmomatic/trimmomatic-0.36/trimmomatic-0.36.jar SE \
--threads 2 -phred33 \ 
-~/rawdata/AX2331_GTTTCGGA_L001_R1_001.fastq \
-~/trimming/AX2331_GTTTCGGA_L001_R1_001_.trimmed.fastq \
-ILLUMINACLIP:/home/a-m/mmsalaz2/trimming/TruSeq3-SE.fa:2:30:10
+-threads 2 -phred33 ~/rawdata/AX2331_GTTTCGGA_L001_R1_001.fastq \
+~/trimming/AX2331_GTTTCGGA_L001_R1_001.trimmed.fastq \
+ILLUMINACLIP:~/trimming/TruSeq3-SE.fa:2:30:10
